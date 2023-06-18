4cbc0d54825a2492757287fbbec67d1c786bba05
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-18 20:13:24 +0200

    font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["*.html"],
   theme: {
     extend: {},
+    fontFamily: {
+      vazir: ["vazir", "san-serif"],
+      vazirBlack: ["vazirBlack", "san-serif"],
+      vazirBold: ["vazirBold", "san-serif"],
+      vazirMed: ["vazirMed", "san-serif"],
+      vazirLight: ["vazirLight", "san-serif"],
+    },
   },
   plugins: [],
 };
